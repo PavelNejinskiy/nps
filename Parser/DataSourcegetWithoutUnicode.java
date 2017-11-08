8ef0957894f8a508deb34da9dce65ae9ef4600b7
PavelNejinskiy <808nps@gmail.com>
2017-11-08 12:02:29 +0200

    Add files via upload

---
+++ Parser/DataSourcegetWithoutUnicode.java
@@ -0,0 +1,39 @@
+package Parser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by мори on 08.11.2017.
+ */
+public class DataSourcegetWithoutUnicode extends FileDataSource {
+
+    private File file;
+
+    public DataSourcegetWithoutUnicode(File file) {
+        super(file);
+        this.file = file;
+    }
+    @Override
+    public String getContent() {
+
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String output = "";
+            int data;
+            while ((output = reader.readLine()) != null) {
+                data = reader.read();
+                if (data < 0x80) {
+                    output += output;
+                }
+            }
+            return output;
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            return "Error";
+        }
+    }
+}
