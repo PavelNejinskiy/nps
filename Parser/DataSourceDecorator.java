1ee57ef2602681887a7f5d511cfa9ed4e14bb8a6
PavelNejinskiy <808nps@gmail.com>
2017-11-08 11:07:02 +0200

    Update DataSourceDecorator.java

--- Parser/DataSourceDecorator.java
+++ Parser/DataSourceDecorator.java
@@ -4,9 +4,13 @@ package Parser;
  * Created by мори on 08.11.2017.
  */
 public class DataSourceDecorator implements InterfaceParser {
+    
+    private InterfaceParser wrapper;
 
-    private FileDataSource wrapper;
-
+    public DataSourceDecorator(InterfaceParser wrapper) {
+        this.wrapper = wrapper;
+    }
+    
     @Override
     public String getContent() {
         return wrapper.getContent();
